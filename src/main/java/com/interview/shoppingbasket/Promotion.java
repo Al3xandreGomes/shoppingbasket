9d43cbc0156dd662ee768716b03d2af7cc8acba7
Alexandre Gomes <alexgomes863@gmail.com>
2023-01-20 13:18:11 +0000

    Improvement by removing magic numbers and added some java doc

--- src/main/java/com/interview/shoppingbasket/Promotion.java
+++ src/main/java/com/interview/shoppingbasket/Promotion.java
@@ -2,6 +2,9 @@ package com.interview.shoppingbasket;
 
 import lombok.*;
 
+/**
+ * The type Promotion.
+ */
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -9,37 +12,59 @@ import lombok.*;
 @AllArgsConstructor
 public class Promotion {
 
+    /**
+     * The constant FIFTY_PERCENTAGE_VALUE.
+     */
+    public static final double FIFTY_PERCENTAGE_VALUE = 0.5;
+    /**
+     * The constant TEN_PERCATANGE_VALUE.
+     */
+    public static final double TEN_PERCATANGE_VALUE = 0.1;
     // Not yet implemented
     private String productCode;
     private PromotionType promotionType;
 
 
+    /**
+     * The enum Promotion type.
+     */
     public enum PromotionType {
+        /**
+         * The Two for one.
+         */
         TWO_FOR_ONE {
             @Override
             double getPromotionValue(BasketItem basketItem, double price) {
-                double discountedPrice = (price * basketItem.getQuantity()) * 0.5;
-                //if quantity is odd needs more 0.5 individual product
+                double discountedPrice = (price * basketItem.getQuantity()) * FIFTY_PERCENTAGE_VALUE;
                 if (basketItem.getQuantity() % 2 == 1) {
-                    discountedPrice += 0.5 * price;
+                    discountedPrice += FIFTY_PERCENTAGE_VALUE * price;
                 }
                 return discountedPrice;
             }
         },
+        /**
+         * The Fifty percent.
+         */
         FIFTY_PERCENT {
             @Override
             double getPromotionValue(BasketItem basketItem, double price) {
-                return (price * basketItem.getQuantity()) * 0.5;
+                return (price * basketItem.getQuantity()) * FIFTY_PERCENTAGE_VALUE;
             }
         },
 
+        /**
+         * The Ten percent.
+         */
         TEN_PERCENT {
             @Override
             double getPromotionValue(BasketItem basketItem, double price) {
-                return price - (price * basketItem.getQuantity() * 0.1);
+                return price - (price * basketItem.getQuantity() * TEN_PERCATANGE_VALUE);
             }
         },
 
+        /**
+         * The Without promotion.
+         */
         WITHOUT_PROMOTION {
             @Override
             double getPromotionValue(BasketItem basketItem, double price) {
@@ -47,6 +72,13 @@ public class Promotion {
             }
         };
 
+        /**
+         * Gets promotion value.
+         *
+         * @param basketItem the basket item
+         * @param price      the price
+         * @return the promotion value
+         */
         abstract double getPromotionValue(BasketItem basketItem, double price);
     }
 
